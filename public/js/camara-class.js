bfc9765af0857e88c9d399f56d2270748bda1fb9
Wilson Martinez <wilsonmb89@hotmail.com>
2020-12-13 16:39:20 -0500

    First commit

---
+++ public/js/camara-class.js
@@ -0,0 +1,41 @@
+class Camara {
+
+  constructor(videoNode) {
+    this.videoNode = videoNode;
+    console.log('Camara class init');
+  }
+
+  encender() {
+    if (navigator.mediaDevices) {
+      navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: { width: 300, height: 300 }
+      }).then(
+        stream => {
+          this.videoNode.srcObject = stream;
+          this.stream = stream;
+        }
+      );
+    }
+  }
+
+  apagar() {
+    this.videoNode.pause();
+
+    if (this.stream) {
+      this.stream.getTracks()[0].stop();
+    }
+  }
+
+  tomarFoto() {
+    let canvas = document.createElement('canvas');
+    canvas.setAttribute('width', 300);
+    canvas.setAttribute('height', 300);
+    let context = canvas.getContext('2d');
+    context.drawImage(this.videoNode, 0, 0, canvas.width, canvas.height);
+    this.foto = context.canvas.toDataURL();
+    canvas = null;
+    context = null;
+    return this.foto;
+  }
+}
